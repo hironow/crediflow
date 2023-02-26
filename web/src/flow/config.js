be0be3e7c3be01711b58ae19bdf7a157f949bdb0
hironow <hironow365@gmail.com>
2023-02-26 18:19:22 +0900

    update config

--- web/src/flow/config.js
+++ web/src/flow/config.js
@@ -2,9 +2,8 @@ import { config } from '@onflow/config';
 
 config({
 	'app.detail.title': 'Crediflow', // Shows user what dapp is trying to connect
-	'app.detail.icon': 'https://unavatar.io/twitter/hironow', // shows image to the user to display your dapp brand
+	'app.detail.icon': 'https://unavatar.io/github/hironow', // shows image to the user to display your dapp brand
 	'accessNode.api': import.meta.env.VITE_ACCESS_NODE_API,
 	'discovery.wallet': import.meta.env.VITE_DISCOVERY_WALLET,
-	'0xProfile': import.meta.env.VITE_PROFILE_ADDRESS,
 	'0xCrediflow': import.meta.env.VITE_CREDIFLOW_ADDRESS
 });
