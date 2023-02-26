1c2c6b44a14d8282af2d3d31a93aa9d1bd083a1a
hironow <hironow365@gmail.com>
2023-02-26 23:34:39 +0900

    update about page

--- web/src/routes/about/+page.svelte
+++ web/src/routes/about/+page.svelte
@@ -3,28 +3,27 @@
 </svelte:head>
 
 <div class="container">
-	<h1>About this app</h1>
+	<h1>About</h1>
 
 	<p>
-		This is a <a href="https://developers.flow.com/fcl">FCL-powered</a> app built on
-		<a href="https://developers.flow.com">Flow</a>.
+		Crediflow combines "Credit" and "Flow" to provide a decentralized platform for creators and
+		admirers to share and reward each other's content.
+	</p>
+
+	<p>
+		This contract is for Crediflow, a decentralized platform on the Flow blockchain that enables
+		creators and admirers to share and reward each other with end credits and tips.
 	</p>
 
 	<p>This app shows how to:</p>
 	<ul>
-		<li>Authenticate a user on the Flow blockchain.</li>
-		<li>
-			Import a contract to let a user create a profile and query the blockchain for their data.
-		</li>
-		<li>Let a user modify their profile and mutate the blockchain to save changes.</li>
+		<li>Anyone can mint NFTs as an Admirer and send tips.</li>
+		<li>As a Creator registered on Crediflow, one can mint NFTs and claim them.</li>
+		<li>It is possible to create one's own Crediflow as a host.</li>
 	</ul>
 
 	<p>
-		Use this app as a starting template to build your very own web3 app, powered on Flow! Flow is a
-		fast, decentralized, and developer-friendly blockchain, designed as the foundation for a new
-		generation of games, apps, and the digital assets that power them. It is based on a unique,
-		multi-role architecture, and designed to scale without sharding, allowing for massive
-		improvements in speed and throughput while preserving a developer-friendly, ACID-compliant
-		environment.
+		The main purpose of Crediflow is to provide a platform for creators to receive recognition for
+		their work and for admirers to show their appreciation and support through tipping.
 	</p>
 </div>
